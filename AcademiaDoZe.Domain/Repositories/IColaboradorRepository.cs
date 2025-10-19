b5ef0bde9bba9610d76d3201181cdfecf15d6839
andrenicsx <@andrenicolasgc@gmail.com>
2025-10-18 23:58:34 -0300

    busca parcial do CPF

--- AcademiaDoZe.Domain/Repositories/IColaboradorRepository.cs
+++ AcademiaDoZe.Domain/Repositories/IColaboradorRepository.cs
@@ -7,7 +7,7 @@ public interface IColaboradorRepository : IRepository<Colaborador>
 {
     // Métodos específicos do domínio
 
-    Task<Colaborador?> ObterPorCpf(string cpf);
+    Task<IEnumerable<Colaborador>> ObterPorCpf(string cpf);
     Task<bool> CpfJaExiste(string cpf, int? id = null);
     Task<bool> TrocarSenha(int id, string novaSenha);
 }
